d708c999676008dd07d517628a34e61e59c6857e
pverma2905 <pverma2905@gmail.com>
2024-08-01 17:38:29 +0530

    first commit

---
+++ src/app/components/grocery/grocery.component.ts
@@ -0,0 +1,58 @@
+import { Component, Signal } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Grocery } from '../../../models/grocery.model';
+import { CommonModule } from '@angular/common';
+import { Store } from '@ngrx/store';
+import { addToBucket, removeFromBucket } from '../../store/actions/bucket.action';
+import { selectGroceries, selectGroceryByType } from '../../store/selectors/grocery.selector';
+
+
+@Component({
+  selector: 'app-grocery',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './grocery.component.html',
+  styleUrl: './grocery.component.css'
+})
+export class GroceryComponent {
+
+  groceries$?:Observable<Grocery[]>;
+  filteredGroceries$?:Observable<Grocery[]>;
+
+  constructor(private store:Store<{groceries:Grocery[]}>){
+     this.groceries$ = store.select("groceries")
+    // this.groceries$ = store.select(selectGroceryByType)
+  }
+
+
+
+  onTypeChange(event: Event){
+    const selectedType = (event.target as HTMLSelectElement).value
+    if(selectedType){
+      this.filteredGroceries$ = this.store.select(selectGroceryByType(selectedType))
+    }  else{
+      this.filteredGroceries$ = undefined
+    }
+  }
+
+
+  increment(item:Grocery){
+    const payload = {
+      id:item.id,
+      name:item.name,
+      quantity:1
+    }
+    
+    this.store.dispatch(addToBucket({payload}))
+
+  }
+  decrement(item:Grocery){
+    const payload = {
+      id:item.id
+    }
+
+    this.store.dispatch(removeFromBucket({payload}))
+
+  }
+
+}
---
+++ src/app/store/selectors/grocery.selector.ts
@@ -0,0 +1,13 @@
+import { createFeatureSelector, createSelector } from "@ngrx/store";
+import { Grocery } from "../../../models/grocery.model";
+
+// export const selectGroceries = (state:{groceries:Grocery[]})=>state.groceries
+
+export const selectGroceries = createFeatureSelector<Grocery[]>('groceries')
+
+export const selectGroceryByType =(type:string)=> createSelector(
+    selectGroceries,
+    (state)=>{
+        return state.filter(item=>item.type===type)
+    }
+)
